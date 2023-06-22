aa2b50f1cee6bf70f0d234b49ced2fdee2491849
gopalnagar <gopalnagarjwr@gmail.com>
2023-06-22 15:06:04 +0530

    create thread with synchronized

---
+++ Multi_thread.java
@@ -0,0 +1,23 @@
+
+class Thread1 extends Thread{
+  synchronized public void run(){
+        for (int i=1; i<=5; i++){
+            System.out.print(i+" ");
+            try {
+                sleep(300);
+            }catch (Exception e){
+                System.out.println("Exception throw");
+            }
+        }
+    }
+}
+
+public class Multi_thread {
+    public static void main(String[] args){
+        Thread1 p=new Thread1();
+        Thread1 p1=new Thread1();
+       p.start();
+       p1.start();
+
+    }
+}
