ff99d6a1c8e6dc2b8b4c5d9f3b617c48ff6c2a8d
gopalnagar <gopalnagarjwr@gmail.com>
2023-06-22 15:00:57 +0530

    create thread with synchronized

--- Main.java
+++ Main.java
@@ -1,48 +1,23 @@
-class A{
-   synchronized void add(int n) throws Exception
-    {
-     for(int i=1; i<=5; i++){
-         System.out.print(i*n+" ");
-             Thread.sleep(1000);
-     }
-    }
-}
-class B extends Thread{
-    A i;
-      B(A i){
-          this.i=i;
-      }
-      public void run(){
-          try {
-              i.add(5);
-          }catch (Exception e){
 
-          }
-
-      }
-}
-class C extends Thread{
-    A i;
-    C(A i){
-        this.i=i;
-    }
-    public void run(){
-        try {
-            i.add(6);
-        }catch (Exception e){
-
+class Thread1 extends Thread{
+  synchronized public void run(){
+        for (int i=1; i<=5; i++){
+            System.out.print(i+" ");
+            try {
+                sleep(300);
+            }catch (Exception e){
+                System.out.println("Exception throw");
+            }
         }
     }
 }
-public class Main {
-    public static void main(String[] args) {
-        A p=new A();
-        B k=new B(p);
-        C k1=new C(p);
 
-        try {
-            k.start();
-            k1.start();
-        }catch (Exception e){}
+public class Main {
+    public static void main(String[] args){
+        Thread1 p=new Thread1();
+        Thread1 p1=new Thread1();
+       p.start();
+       p1.start();
+
     }
 }
